83e062e309ccfee20517dd772d1fd00cb5093663
Reman Child <rchild@okta.com>
2016-11-06 16:46:15 -0800

    foo

--- app/app.js
+++ app/app.js
@@ -1,24 +1,3 @@
-/**
- * TODO:
- * x Pass config to app so that it can trickle down?
- * x Is it possible to not include <app></app> on the page?
- *   x else we need to be able to pass per type template...
- * x Update webpack to actually bundle as bundle.js, and have main file be
- *   called app in base stuff
- * x Rename "app" to "profile" in base stuff
- * x Rename "start" to "bootstrap"
- * x Add user info to template
- *
- * - Copy changes to generator
- * - Get mock-okta working...?
- * - Get selenium tests working
- *
- * TODO ON GENERATOR:
- * - Rename "app" to "profile" in app
- * - Rename webpack "app" to "bundle"
- * - Rename app.start to bundle.bootstrap
- */
-
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import AppComponent from './app.component';
