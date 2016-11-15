6a20f2bb70cbb9fc2e715a10561e37c33a6814c4
Reman Child <rchild@okta.com>
2016-11-14 20:47:30 -0800

    wip

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash -vx
 
-cd ${OKTA_HOME}/${REPO}
+source $OKTA_HOME/$REPO/scripts/setup.sh
 
 export TEST_SUITE_TYPE="checkstyle"
 export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/lint"
-
-if ! npm install; then
-  echo "npm install failed! Exiting..."
-  exit ${FAILED_SETUP}
-fi
 
 if ! npm run lint:report; then
   echo "lint failed! Exiting..."
