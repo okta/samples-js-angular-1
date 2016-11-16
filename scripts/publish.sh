9626794d69a508180a33c38967e2c938a69ab98d
Reman Child <rchild@okta.com>
2016-11-15 19:25:04 -0800

    :seedling: Use tasks for publishing in the promote flow
    RELATED TO OKTA-108137

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,17 +1,26 @@
 #!/bin/bash -vx
 
-REGISTRY="https://artifacts.aue1d.saasure.com/artifactory/api/npm/npm-okta"
-
 source $OKTA_HOME/$REPO/scripts/setup.sh
+
+REGISTRY="https://artifacts.aue1d.saasure.com/artifactory/api/npm/npm-okta"
 
 export TEST_SUITE_TYPE="build"
 
-if ! npm run ci-update-package -- --branch ${BRANCH}; then
+if [ -n "$action_branch" ];
+then
+  echo "Publishing from bacon task using branch $action_branch"
+  TARGET_BRANCH=$action_branch
+else
+  echo "Publishing from bacon testSuite using branch $BRANCH"
+  TARGET_BRANCH=$BRANCH
+fi
+
+if ! npm run ci-update-package -- --branch ${TARGET_BRANCH}; then
   echo "ci-update-package failed! Exiting..."
   exit $FAILED_SETUP
 fi
 
-if ! npm run build -- --branch ${BRANCH}; then
+if ! npm run build -- --branch ${TARGET_BRANCH}; then
   echo "run run build failed! Exiting..."
   exit $FAILED_SETUP
 fi
