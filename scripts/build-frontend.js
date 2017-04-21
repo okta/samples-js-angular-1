c34826d7f71807b4f9a53d3262188e20e6fa139f
Abhijeet Pawar <abhijeetkpawar@gmail.com>
2017-04-21 14:19:31 +0530

    Windows build fix
    Fix for bug #18

--- scripts/build-frontend.js
+++ scripts/build-frontend.js
@@ -20,4 +20,5 @@ const exec = require('child_process').execSync;
 const config = require(path.resolve(cwd, '.samples.config.json'));
 const out = path.resolve(cwd, config.oktaSample.server.staticDir);
 
-exec(`DIST_OUT=${out} npm run build`, { cwd: __dirname, stdio: 'inherit' });
+process.env.DIST_OUT = out;
+exec(`npm run build`, { cwd: __dirname, stdio: 'inherit' });
