b23a00b9d0dadf90a4ece66bedb5b45c9a739ad9
jmelberg <jordan.melberg@okta.com>
2017-08-14 13:20:24 -0700

    :seedling: Adds support for default authorization server

--- app/components/authorization-code/login-custom/login-custom.controller.js
+++ app/components/authorization-code/login-custom/login-custom.controller.js
@@ -24,6 +24,7 @@ class LoginCustomController {
       clientId: this.config.clientId,
       redirectUri: this.config.redirectUri,
       authParams: {
+        issuer: this.config.issuer,
         responseType: 'code',
         scopes: ['openid', 'email', 'profile'],
       },
