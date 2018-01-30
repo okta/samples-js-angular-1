3357a49fbc2fd999481d63d07a47f22606ac39cd
Robert Damphousse <robert.damphousse.devex@okta.com>
2018-01-30 14:53:30 -0800

    Only run selenium on Travis

--- scripts/selenium.sh
+++ scripts/selenium.sh
@@ -4,14 +4,6 @@ source $OKTA_HOME/robo-warrior/setupfiles/google-chrome-stable/google-chrome-sta
 source $OKTA_HOME/robo-warrior/setupfiles/xvfb/xvfb-entrypoint.sh start
 source $OKTA_HOME/$REPO/scripts/setup.sh
 
-export TEST_SUITE_TYPE="junit"
-export TEST_RESULT_FILE_DIR="${REPO}/build2/reports/junit"
+echo "Selenium tests for this repo are now run through Travis"
 
-if ! npm test; then
-  echo "npm test failed! Exiting..."
-  exit ${TEST_FAILURE}
-fi
-
-echo $TEST_SUITE_TYPE > $TEST_SUITE_TYPE_FILE
-echo $TEST_RESULT_FILE_DIR > $TEST_RESULT_FILE_DIR_FILE
 exit $PUBLISH_TYPE_AND_RESULT_DIR;
