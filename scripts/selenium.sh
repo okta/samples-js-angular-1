123a8789afc8c5b3c925d516388c052042c696bb
Robert Damphousse <robert.damphousse.devex@okta.com>
2018-01-31 13:53:56 -0800

    chore: slim down selenium placeholder script

--- scripts/selenium.sh
+++ scripts/selenium.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -vx
 
-source $OKTA_HOME/robo-warrior/setupfiles/google-chrome-stable/google-chrome-stable-setup.sh 53.0.2785.143-1
-source $OKTA_HOME/robo-warrior/setupfiles/xvfb/xvfb-entrypoint.sh start
-source $OKTA_HOME/$REPO/scripts/setup.sh
-
 echo "Selenium tests for this repo are now run through Travis"
 
 exit 0;
